c6b809244a228b2b3d1eb817c4f3b877883eb06a
Yau <christy.yau@sap.com>
2019-03-04 18:33:05 -0800

    added 4 animated clouds

--- src/components/background.js
+++ src/components/background.js
@@ -1,11 +1,12 @@
 import React from 'react';
 // import landscape_img from '../../assets/images/after-vinyl---export-projections.jpg';
-// import landscape_img from '../../assets/images/Background.jpg';
-import landscape_img from '../../assets/images/projected.jpg';
+//import landscape_img from '../../assets/images/Background.jpg';
+import landscape_img from '../../assets/images/projectednew.jpg';
 import ic from '../../assets/images/9-permanent.png';
 import bubbles1 from '../../assets/images/bubbles/bubbles1.png';
 import bubbles2 from '../../assets/images/bubbles/bubbles2.png';
 import bubbles3 from '../../assets/images/bubbles/bubbles3.png';
+import cloud1 from '../../assets/images/anim/cloud.png';
 
 
 const Background = () => {
@@ -16,6 +17,10 @@ const Background = () => {
 			<img src={ bubbles2 } className="bubblegroup2"  />
 			<img src={ bubbles3 } className="bubblegroup3"  />
 			<img src={ ic } className="ic"  />
+			<img src= { cloud1 } className="cloud1"/>
+			<img src= { cloud1 } className="cloud2"/>
+			<img src= { cloud1 } className="cloud3"/>
+			<img src= { cloud1 } className="cloud4"/>
 		</div>
 	);
 }
