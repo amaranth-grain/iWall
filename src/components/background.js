6f4cb0b830d3496cc7e0e805edbdf5e290aaa1fc
Chris Liu <chris.liu03@sap.com>
2019-03-12 17:22:11 -0700

    mario, yoshi, flag, elevator anim added

--- src/components/background.js
+++ src/components/background.js
@@ -7,6 +7,17 @@ import bubbles1 from '../../assets/images/bubbles/bubbles1.png';
 import bubbles2 from '../../assets/images/bubbles/bubbles2.png';
 import bubbles3 from '../../assets/images/bubbles/bubbles3.png';
 import cloud1 from '../../assets/images/anim/cloud.png';
+import cloudFilled from '../../assets/images/anim/cloud_filled.png';
+import cloudFilled2 from '../../assets/images/anim/cloud_2_filled.png';
+import cloudFilled3 from '../../assets/images/anim/cloud_3_filled.png';
+import cloudFilled4 from '../../assets/images/anim/cloud_4_filled.png';
+import elevator from '../../assets/images/anim/Elevator.png';
+import flag from '../../assets/images/anim/Flag_Fill.png'; 
+import wave from '../../assets/images/anim/wave.png';
+
+// FOR FUN
+import mario from '../../assets/images/anim/mario.png';
+import yoshi from '../../assets/images/anim/yoshi.png';
 
 
 const Background = () => {
@@ -22,6 +33,11 @@ const Background = () => {
 			<img src= { cloud1 } className="cloud2"/>
 			<img src= { cloud1 } className="cloud3"/>
 			<img src= { cloud1 } className="cloud4"/>
+			<img src= { elevator } className="elevator" />
+			<img src= { flag } className="flag" />
+			<img src= { wave } className="wave" />
+			<img src= { mario } className="mario" />
+			<img src= { yoshi } className="yoshi" />
 		</div>
 	);
 }
