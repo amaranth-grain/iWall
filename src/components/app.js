290812c8f1e87cd5f6b0e8f5c62d0abbaf74e3d4
Chris Liu <chris.liu03@sap.com>
2019-03-19 21:39:35 -0700

    swing posistion

--- src/components/app.js
+++ src/components/app.js
@@ -105,8 +105,8 @@ export default class App extends Component {
 
 		//comment contents for componenet placement
 		removeImage = (img) => {
-			// const newImages = this.state.images.filter(image => image.imageName !== img.imageName);
-			// this.setState({ images: newImages });
+			const newImages = this.state.images.filter(image => image.imageName !== img.imageName);
+			this.setState({ images: newImages });
 		}
 
 		convertSerialPortDataToJSX = (data) => {
--- src/components/background.js
+++ src/components/background.js
@@ -15,19 +15,20 @@ import elevator from '../../assets/images/anim/Elevator.png';
 import flag from '../../assets/images/anim/Flag_Fill.png';
 import wave from '../../assets/images/anim/wave.png';
 import guitarist from '../../assets/images/people/guitarist.png';
+import swing from '../../assets/images/people/swing.png';
 
 // FOR FUN
 //import mario from '../../assets/images/anim/mario.png';
 //import yoshi from '../../assets/images/anim/yoshi.png';
 // import mario from '../../assets/images/anim/mario.png';
 // import yoshi from '../../assets/images/anim/yoshi.png';
+// <img src={ bubbles2 } className="bubblegroup2"  />
 
 const Background = () => {
 	return (
 		<div >
 			<img src={ landscape_img } className="artboard-background"  />
 			<img src={ bubbles1 } className="bubblegroup1"  />
-			<img src={ bubbles2 } className="bubblegroup2"  />
 			<img src={ bubbles3 } className="bubblegroup3"  />
 			<img src={ ic } className="ic"  />
 			<img src= { cloud1 } className="cloud1"/>
@@ -39,6 +40,7 @@ const Background = () => {
 			<img src= { flag } className="flag" />
 			<img src= { wave } className="wave" />
 			<img src= { guitarist } className="guitarist" />
+			<img src= { swing } className="swing" />
 		</div>
 	);
 }
